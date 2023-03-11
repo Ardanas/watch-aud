42097fca0c0d268c8b9345538f841977469e0ef9
Ardanas <ardans@foxmail.com>
2023-03-11 11:33:43 +0800

    feat: update config

--- index.js
+++ index.js
@@ -2,23 +2,29 @@ const axios = require('axios');
 const nodemailer = require('nodemailer');
 const cron = require('node-cron');
 
-const API_KEY = process.env.API_KEY; // ixer.io API key
-const CURRENCY_PAIR = process.env.CURRENCY_PAIR || 'AUDCNY'; // 'AUDCNY'; // AUD/CNY exchange rate
-const EMAIL = process.env.EMAIL; // your email address
-const PASSWORD = process.env.PASSWORD; // your email password
-const TO_EMAIL = process.env.TO_EMAIL; // recipient email address
-const THRESHOLD = process.env.THRESHOLD; // exchange rate threshold
+const API_KEY = process.env.API_KEY || 'xxx'; // ixer.io API key
+const EMAIL = process.env.EMAIL || 'xxx'; // your email address
+const PASSWORD = process.env.PASSWORD || 'xxx'; // your email password
+const TO_EMAIL = process.env.TO_EMAIL || 'xxx'; // recipient email address
+const THRESHOLD = process.env.THRESHOLD || 4.56; // exchange rate threshold
+
 
 async function getExchangeRate() {
-    const url = `http://data.fixer.io/api/latest?access_key=${API_KEY}&symbols=${CURRENCY_PAIR}`;
-    const response = await axios.get(url);
-    const rate = response.data.rates[CURRENCY_PAIR];
+    const url = `https://api.apilayer.com/exchangerates_data/latest?symbols=CNY&base=AUD&access_key${API_KEY}`;
+    const response = await axios.get(url, {
+        headers: {
+            'apikey': API_KEY,
+        }
+    });
+    const rate = response.data.rates.CNY
     return rate;
 }
 
 async function sendEmail(rate) {
     const transporter = nodemailer.createTransport({
-        service: 'gmail',
+        host: 'smtp.qq.com',
+        port: 465,
+        secure: true,
         auth: {
             user: EMAIL,
             pass: PASSWORD,
@@ -29,17 +35,29 @@ async function sendEmail(rate) {
         from: EMAIL,
         to: TO_EMAIL,
         subject: 'AUD/CNY Exchange Rate Alert',
-        text: `The exchange rate is ${rate.toFixed(2)}, which is lower than 4.6!`,
+        text: `The exchange rate is ${rate.toFixed(2)}, which is lower than ${THRESHOLD}!`,
     };
+    transporter.sendMail(mailOptions, (error, info) => {
 
-    const info = await transporter.sendMail(mailOptions);
-    console.log(`Email sent: ${info.messageId}`);
+        console.log(`Email sent: ${info.messageId}`);
+
+    });
 }
 
 cron.schedule('* * * * *', async () => {
     const rate = await getExchangeRate();
     console.log(`AUD/CNY exchange rate: ${rate.toFixed(2)}`);
-    if (rate < THRESHOLD) {
+    console.log(typeof rate);
+    if (Number(rate) < THRESHOLD) {
+        console.log('email');
         await sendEmail(rate);
     }
 });
+
+// (async () => {
+//     const rate = await getExchangeRate();
+//     console.log(`AUD/CNY exchange rate: ${rate.toFixed(2)}`);
+//     if (Number(rate) < THRESHOLD) {
+//         await sendEmail(rate);
+//     }
+// })()
